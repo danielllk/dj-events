b8b6f01c77198d8c7d416d0bebea012fffaaba5f
danielllk <danielk2091@gmail.com>
2023-02-20 16:36:53 +0000

    first commit

---
+++ dj-events-frontend/src/pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>about</h1>
+      <p>This app is an app to find the latest DJ and other musical events</p>
+      <p>version 1.0.0</p>
+    </div>
+  );
+}
---
+++ dj-events-frontend/src/pages/events/add.js
@@ -0,0 +1,3 @@
+export default function AddEventPage() {
+  return <div>AddEvent</div>;
+}
